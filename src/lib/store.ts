f2d6564003e1271394f3e76059a7a37ef954afdd
latttchc <iam.yuk2882@gmail.com>
2025-09-05 15:52:35 +0900

    タスクの非同期取得機能を追加し、Reduxストアの状態管理を強化

--- src/lib/store.ts
+++ src/lib/store.ts
@@ -1,24 +1,32 @@
 import type { TaskData } from "../types";
-import { configureStore, createSlice, PayloadAction } from "@reduxjs/toolkit";
+import { configureStore, createSlice, createAsyncThunk, PayloadAction } from "@reduxjs/toolkit";
 
 interface TaskBoxState {
   tasks: TaskData[];
-  status: 'idle' | 'loading' | 'failed';
+  status: 'idle' | 'loading' | 'failed' | 'succeeded';
   error: string | null
 }
 
-const defaultTasks: TaskData[] = [
-  { id: '1', title: 'Something', state: 'TASK_INBOX' },
-  { id: '2', title: 'Something more', state: 'TASK_INBOX' },
-  { id: '3', title: 'Something else', state: 'TASK_INBOX' },
-  { id: '4', title: 'Something again', state: 'TASK_INBOX' },
-]
-
 const TaskBoxData: TaskBoxState = {
-  tasks: defaultTasks,
+  tasks: [],
   status: 'idle',
   error: null,
 }
+
+export const fetchTasks = createAsyncThunk('taskbox/fetchTasks', async () => {
+  const reponse = await fetch(
+    'https://jsonplaceholder.typicode.com/todos?userId=1'
+  );
+  const data = await reponse.json();
+  const result = data.map(
+    (task: { id: number; title: string; completed: boolean }) => ({
+      id: `${task.id}`,
+      title: task.title,
+      state: task.completed ? 'TASK_ARCHIVED' : 'TASK_INBOX',
+    })
+  );
+  return result
+})
 
 const TasksSlice = createSlice({
   name: 'taskbox',
@@ -33,6 +41,24 @@ const TasksSlice = createSlice({
         task.state = action.payload.newTaskState
       }
     },
+  },
+  extraReducers(builder) {
+    builder
+      .addCase(fetchTasks.pending, (state) => {
+        state.status = 'loading';
+        state.error = null;
+        state.tasks = [];
+      })
+      .addCase(fetchTasks.fulfilled, (state, action) => {
+        state.status = 'succeeded';
+        state.error = null;
+        state.tasks = action.payload;
+      })
+      .addCase(fetchTasks.rejected, (state) => {
+        state.status = 'failed'
+        state.error = 'Something went wrong'
+        state.tasks = []
+      })
   },
 });
 
