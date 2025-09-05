cdb3eebba822e1dd2a8841364295359c79bf7a06
latttchc <iam.yuk2882@gmail.com>
2025-09-05 14:53:10 +0900

    TaskListコンポーネントをエクスポートし、ローディング状態と空の状態の表示を改善

--- src/components/TaskList.tsx
+++ src/components/TaskList.tsx
@@ -9,7 +9,7 @@ interface TaskListProps {
   onArchiveTask: (id: string) => void;
 };
 
-const TaskList = ({
+export const TaskList = ({
   loading = false,
   tasks,
   onPinTask,
@@ -20,21 +20,53 @@ const TaskList = ({
     onArchiveTask,
   };
 
+  const LoadingRow = (
+    <div className="loading-item">
+      <span className="glow-checkbox" />
+      <span className="glow-text">
+        <span>Loading</span>
+        <span>cool</span>
+        <span>state</span>
+      </span>
+    </div>
+  )
+
   if (loading) {
-    return <div className="list-item">loading</div>
+    return (
+      <div className="list-item" data-testid="loading" key={"loading"}>
+        {LoadingRow}
+        {LoadingRow}
+        {LoadingRow}
+        {LoadingRow}
+        {LoadingRow}
+        {LoadingRow}
+      </div>
+    )
   }
 
   if (tasks.length === 0) {
-    return <div className="list-item">empty</div>
+    return (
+      <div className="list-items" key={"empty"} data-testid="empty">
+        <div className="wrapper-message">
+          <span className="icon-check" />
+          <p className="title-message">You have no tasks</p>
+          <p className="subtitle-message">Sit back and relax</p>
+        </div>
+      </div>
+    );
   }
 
+  const tasksInOrder = [
+    ...tasks.filter((t) => t.state === "TASK_PINNED"),
+    ...tasks.filter((t) => t.state !== "TASK_PINNED"),
+  ];
   return (
-    <div className="list-item">
-      {tasks.map((task) => (
+    <div className="list-items">
+      {tasksInOrder.map((task) => (
         <Task key={task.id} task={task} {...events} />
       ))}
     </div>
   );
 }
 
-export default TaskList
+
